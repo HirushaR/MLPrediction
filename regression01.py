2b667e5c6412e3b474ff7e56be66525e94c19d3c
HirushaR <hirusharandunu11@gmail.com>
2019-09-22 13:43:24 +0530

    first

--- regression01.py
+++ regression01.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import math
 import quandl
+import numpy as np
+from sklearn import preprocessing,svm,model_selection
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL")
 # print(df.head())
@@ -10,9 +13,27 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
+
 df.fillna(-99999, inplace=True)
 forecast_Out = int(math.ceil(0.01*len(df)))
-df['lable'] = df[forecast_col].shift(-forecast_Out)
+#print(forecast_Out)
+
+df['label'] = df[forecast_col].shift(-forecast_Out)
 df.dropna(inplace=True)
-print(df.head())
+#print(df.head())
 #print(df.tail())
+
+X = np.array(df.drop(['label'],1))
+Y = np.array(df['label'])
+X = preprocessing.scale(X)
+Y = np.array(df['label'])
+
+#print(len(X), len(Y))
+
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2)
+clf = LinearRegression(n_jobs=10)
+clf.fit(X_train, Y_train)
+accuracy = clf.score(X_test, Y_test)
+
+print(accuracy)
+
